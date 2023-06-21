2b70d3fb91aeedcf4dc294f2be00ac90c178b841
santiago <santiagovillahermosa@gmail.com>
2023-06-21 08:49:02 -0400

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
       <div className="App">
-        <BrowserRouter basename="vite_test">
+        <HashRouter basename="vite_test">
           <Routes>
             <Route path="/" element={<div>hello</div>} />
             <Route path="/projects" element={<div>projec</div>} />
             <Route path="projects/:projectName" element={<div>dynamic</div>} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
   );
 }
