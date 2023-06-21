f0cc099044aab005108b2a5984806ad3bf9e0913
santiago <santiagovillahermosa@gmail.com>
2023-06-21 08:37:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>hello</div>} />
+            <Route path="/projects" element={<div>projec</div>} />
+            <Route path="projects/:projectName" element={<div>dynamic</div>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+  );
+}
