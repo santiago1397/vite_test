a5f1ada66efed2453e142fffeae4e20d40721910
santiago <santiagovillahermosa@gmail.com>
2023-06-21 09:13:33 -0400

    6

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite_test"
-})
+  basename: "/vite_test/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
+});
