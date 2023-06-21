33b82ac80789d6320e94efb2976931e7931a2a61
santiago <santiagovillahermosa@gmail.com>
2023-06-21 09:21:40 -0400

    7

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basename: "/vite_test/",
   build: {
     rollupOptions: {
       input: {
