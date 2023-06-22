c8ae7bf324404da47fc72f53a9f5fcf51cdb7ba1
santiago <santiagovillahermosa@gmail.com>
2023-06-21 21:14:22 -0400

    27 netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createHashRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { 
+createHashRouter, 
+createBrowserRouter, 
+RouterProvider,
+BrowserRouter as Router,
+Routes,
+Route 
+}from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/hello" element={<div>hello</div>} />
+        <Route path="/hello/:okay" element={<div>okay</div>} />
+        
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
 
 const router = createBrowserRouter([
   {
@@ -23,16 +43,16 @@ const router = createBrowserRouter([
   }
 
 
-],{ basename: "/vite_test/"})
+],)
 
 /* ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 ); */
 
 
-const rootElement = document.getElementById('root');
+/* const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<RouterProvider router={router} />);
+root.render(<RouterProvider router={router} />); */
 
 
